d5871e63cf11d35c7799a1d6ea6b7413418762d9
Ahmed Abu <ahmedabu13@gmail.com>
2023-01-12 16:12:49 -0600

    Delete Route

--- server/routes/route.js
+++ server/routes/route.js
@@ -5,7 +5,24 @@ const pool = require('../modules/pool.js');
 // WHERE WE PUT OUR ROUTES
 
 
-
+router.delete("/:id", (req, res) => {
+    console.log(req.params);
+    let idToDelete = req.params.id;
+    let sqlQuery = `
+            DELETE FROM "shoppingList"
+            WHERE "id"=$1
+          `;
+    let sqlValues = [idToDelete];
+    pool
+      .query(sqlQuery, sqlValues)
+      .then((dbRes) => {
+        res.sendStatus(200);
+      })
+      .catch((dbErr) => {
+        console.log("broke in Delete route", dbErr);
+      });
+  });
+  
 
 // END OF THE ROUTES
 module.exports = router;
