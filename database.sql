786c2b6981dc2936958bdb935de8c4415a2b42dd
Cole Dahlen <coledahlen@gmail.com>
2023-01-12 14:24:20 -0600

    completed database config

--- database.sql
+++ database.sql
@@ -1,2 +1,16 @@
 -- Don't forget to add your create table SQL 
+CREATE TABLE "shoppingList" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(80),
+  "quantity" numeric(10,2),
+  "unit" VARCHAR(20),
+  "bought" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO "shoppingList"
+	("name", "quantity", "unit")
+	VALUES
+	('Cole', '5', 'logs'),
+	('Connor', '6', 'computers');
+
 -- It is also helpful to include some test data
