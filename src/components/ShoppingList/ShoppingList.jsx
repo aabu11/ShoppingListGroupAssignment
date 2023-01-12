b326df786320dc51ada4fbd3a14e80017862164f
Cole Dahlen <coledahlen@gmail.com>
2023-01-12 16:54:03 -0600

    little more work to do for css

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,9 +1,13 @@
+import './ShoppingList.css';
 function ShoppingList(props){
         return(
             <>
                 <div>
+                    <div id='shoppingTitle'>Shopping List</div>
+                    <button id='resetBtn'>Reset</button>
+                    <button id='clearBtn'>Clear</button>
                     {props.shoppingList.map(shopping => (
-                        <div key={shopping.id}>
+                        <div className='shoppingList' key={shopping.id}>
                             <div>{shopping.name}</div>
                             <div>{String(shopping.quantity)}</div>
                             <div>{shopping.unit}</div>
